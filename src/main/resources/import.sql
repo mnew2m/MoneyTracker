418d9b85f978fd684b08746d206a0f79eadd1a79
mnew2m <mnew2m@gmail.com>
2023-01-11 00:48:54 +0900

    JPA 기초 데이터 Insert 적용

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into com_category(cat_id, cat_name) values('CAT_001', '식비');
+insert into com_category(cat_id, cat_name) values('CAT_002', '교통비');
+insert into com_category(cat_id, cat_name) values('CAT_003', '꾸밈비');
+insert into com_category(cat_id, cat_name) values('CAT_004', '통신비');
+insert into com_payment(pay_id, pay_name) values('PAY_001', '현금');
